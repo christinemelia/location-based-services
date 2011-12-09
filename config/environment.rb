d05571eb0e0356d0fd2e9f2034e8a6c340b4f429
Christine <christinemelia@gmail.com>
2011-12-09 17:03:11 +0000

    hello

---
+++ config/environment.rb
@@ -0,0 +1,12 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Locations::Application.initialize!
+
+#### 2. Add this line to your environment.rb 
+
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Locations::Application.config.secret_token = 'ac1a0e6bc84e7a347a541d14bcb0dfd9170681bc3e424447b95694cf23012d159ce43aa5f2e77ffa3341e0d17669a65dd6ce4c348d2b05407982902940cece74'
